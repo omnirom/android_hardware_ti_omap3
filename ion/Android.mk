6db749a03696de6917a317821a615e5256dae06a
Alexander Martinz <eviscerationls@gmail.com>
2013-12-02 16:34:57 +0100

    Rename iontest to iontest_ti
    Change-Id: I05df124f07d0f915883dc5ecb5b5742ea3077ad1

--- ion/Android.mk
+++ ion/Android.mk
@@ -12,7 +12,7 @@ include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ion.c ion_test.c
-LOCAL_MODULE := iontest
+LOCAL_MODULE := iontest_ti
 LOCAL_MODULE_TAGS := optional tests
 LOCAL_SHARED_LIBRARIES := liblog
 include $(BUILD_HEAPTRACKED_EXECUTABLE)
